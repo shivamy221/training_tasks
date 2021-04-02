205d84c9d090cb7303783ef54ca517fc1e91ac0c
shivamy221 <shivamy221@gmail.com>
2021-04-02 18:55:14 +0530

    Add Task-2 to respective folder

---
+++ Task-2-28-08-19/src/task2/ATMCard.java
@@ -0,0 +1,16 @@
+package task2;
+
+public interface ATMCard {
+	
+	public void balanceEnquiry();
+
+	public void withdrawl();
+
+	public void deposit();
+
+	public void transfer();
+
+	public void changePin();
+
+
+}
