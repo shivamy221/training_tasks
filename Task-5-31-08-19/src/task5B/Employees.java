3105208567b255986fe7ab577d9c5304d5474bf6
shivamy221 <shivamy221@gmail.com>
2021-04-02 19:11:48 +0530

    Add Task-5 to respective folder

---
+++ Task-5-31-08-19/src/task5B/Employees.java
@@ -0,0 +1,66 @@
+package task5B;
+public class Employees {
+
+	public long eno;
+	public String ename;
+	public int salary;
+	public String designation;
+	public String department;
+	
+	public Employees(long eno, String ename, int salary, String designation, String department) {
+		super();
+		this.eno = eno;
+		this.ename = ename;
+		this.salary = salary;
+		this.designation = designation;
+		this.department = department;
+	}
+
+	public long getEno() {
+		return this.eno;
+	}
+
+	public void setEno(long eno) {
+		this.eno = eno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDepartment() {
+		return this.department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employees [eno=" + eno + ", ename=" + ename + ", salary=" + salary + ", designation=" + designation
+				+ ", department=" + department + "]";
+	}
+	
+}
+
